10d3c10d5956c7b8051d3e0b272ef02996ac4a7c
mohanajuhi166 <mohanajuhi166@gmail.com>
2024-04-02 16:59:46 +0200

    Update gen_docker_image.sh

--- llmops/common/scripts/gen_docker_image.sh
+++ llmops/common/scripts/gen_docker_image.sh
@@ -63,7 +63,7 @@ if [[ -n "$selected_object" ]]; then
     result_string=""
 
     for name in "${connection_names[@]}"; do
-        api_key=$(echo $CONNECTION_DETAILS | jq -r --arg name "$name" '.[] | select(.name == $name) | .api_key')
+        api_key=$(echo $connection_details | jq -r --arg name "$name" '.[] | select(.name == $name) | .api_key')
         uppercase_name=$(echo "$name" | tr '[:lower:]' '[:upper:]')
         modified_name="${uppercase_name}_API_KEY"
         result_string+=" -e $modified_name=$api_key"
