fac71d02bc9c1777cf6fb90eaef19d9cd0f93ac8
mohanajuhi166 <mohanab@microsoft.com>
2024-05-22 10:46:36 +0200

    fabric onelake setting up sp authentication

--- dataops/common/fabric_data_store.py
+++ dataops/common/fabric_data_store.py
@@ -39,7 +39,7 @@ def register_data_store(
         description=description,
         one_lake_workspace_name=onelake_workspace_name,
         endpoint=onelake_endpoint,
-        credentials=ServicePrincipalConfiguration(clientId=os.getenv('CLIENT_ID'),
+        credentials=ServicePrincipalConfiguration(client_id="681c93c1-6376-4beb-9d29-cc56131d0a12",
                                                   client_secret=os.getenv('CLIENT_SECRET'),
                                                   tenant_id="3c863c9b-2221-4236-88c3-37fe9e1d06f8"),
         artifact=OneLakeArtifact(
