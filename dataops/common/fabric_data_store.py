ca4cfb41b8d274fd6ee3a0419ca986a5086d0a01
mohanajuhi166 <mohanab@microsoft.com>
2024-07-17 16:20:08 +0200

    testing file upload via sp

--- dataops/common/fabric_data_store.py
+++ dataops/common/fabric_data_store.py
@@ -90,7 +90,8 @@ def register_data_store(
     for path in paths:
         print(path.name + '\n')
 
-    file_system_client.create_file(file="datalakehousetest.Lakehouse")
+    file_client = service_client.get_file_client("datalakehousetest.Lakehouse/Files/")
+    file_client.create_file(file="text.txt")
     #upload_file_to_directory(file_system_client, "dataops/common", "test.txt")
 
     ## AI Client
