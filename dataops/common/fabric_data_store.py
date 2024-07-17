08648df4272d471a59e62f265cd343891d2c5614
mohanajuhi166 <mohanab@microsoft.com>
2024-07-17 13:47:58 +0200

    testing file upload via sp

--- dataops/common/fabric_data_store.py
+++ dataops/common/fabric_data_store.py
@@ -90,7 +90,7 @@ def register_data_store(
     for path in paths:
         print(path.name + '\n')
 
-    upload_file_to_directory(file_system_client, "dataops/common", "test.csv")
+    upload_file_to_directory(file_system_client, "dataops/common", "test.txt")
 
     ## AI Client
 
