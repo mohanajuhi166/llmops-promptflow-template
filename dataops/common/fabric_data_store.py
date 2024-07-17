1709ffa3acabd6e65fac7dd7ea1bee3e2601f37b
mohanajuhi166 <mohanab@microsoft.com>
2024-07-17 14:57:55 +0200

    testing file upload via sp

--- dataops/common/fabric_data_store.py
+++ dataops/common/fabric_data_store.py
@@ -86,7 +86,7 @@ def register_data_store(
 
     # List a directory within the filesystem
     paths = file_system_client.get_paths(path=DATA_PATH)
-    file_system_client.create_file(file="test.txt", content="hello world")
+    file_system_client.create_file(file="test.txt", type="hello world")
 
     for path in paths:
         print(path.name + '\n')
